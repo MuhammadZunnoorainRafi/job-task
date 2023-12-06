7666e7941bc65d7482dbaa050a728a05ea0eef28
Muhammad Zunnoorain Rafi <bhoshyl@gmail.com>
2023-12-06 23:55:27 +0500

    Frontend setup continued

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import '@radix-ui/themes/styles.css';
+
+import { Theme } from '@radix-ui/themes';
+import {
+  RouterProvider
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom';
+import SignIn from './pages/SignIn.tsx';
+import SignUp from './pages/SignUp.tsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<App />}>
+    <Route path='/signUp' element={<SignUp />} />
+    <Route path='/signIn' element={<SignIn />} />
+  </Route>)
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Theme>
+     <RouterProvider router={router} />
+    </Theme>
+  </React.StrictMode>
+);
