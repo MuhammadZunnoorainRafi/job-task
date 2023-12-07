10b11cc71d950f0f7a14d7f835a6e3d5ebcd0743
Muhammad Zunnoorain Rafi <bhoshyl@gmail.com>
2023-12-07 12:26:26 +0500

    task create done

--- frontend/src/hooks/taskQueryHooks.ts
+++ frontend/src/hooks/taskQueryHooks.ts
@@ -1,13 +1,28 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 import { ITask } from '../utils/types';
+import { useAppSelector } from './RTKhooks';
+
+export const useGetTaskQuery = () => {
+  return useQuery({
+    queryKey: ['tasks'],
+    queryFn: async () => {
+      const res = await axios.get('/api/task/get');
+    },
+  });
+};
 
 export const useCreateTaskQuery = () => {
+  const { user } = useAppSelector((store) => store.authReducer);
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: async (data: ITask) => {
-      await axios.post('/api/task/post', data);
+      await axios.post('/api/task/post', data, {
+        headers: {
+          Authorization: `Bearer ${user?.token}`,
+        },
+      });
     },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['tasks'] });
