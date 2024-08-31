76521a2dd91e2658862b35326529e391b2c54d9b
Zunnoorain Rafi <zunnoorainrafi236@gmail.com>
2024-08-31 20:20:02 +0500

    infinite scrolling and optimistic UI completed

--- frontend/src/pages/Tasks.tsx
+++ frontend/src/pages/Tasks.tsx
@@ -2,13 +2,35 @@ import TaskForm from '../components/TaskForm';
 import { useGetTaskQuery } from '../hooks/taskQueryHooks';
 import TaskUI from '../components/TaskUI';
 import { TaskStats } from '../utils/types';
-import { useAutoAnimate } from '@formkit/auto-animate/react';
 import { TbLoader } from 'react-icons/tb';
+import { useInView } from 'react-intersection-observer';
+import { useEffect } from 'react';
 
 function Tasks() {
-  const { data, isPending } = useGetTaskQuery();
-  console.log(data);
-  const [parent] = useAutoAnimate();
+  const {
+    data,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+    isLoading,
+    isError,
+  } = useGetTaskQuery();
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [inView, fetchNextPage]);
+
+  if (isError) {
+    return (
+      <p className="text-center py-56 text-red-500 font-mono tracking-widest text-lg">
+        Error while fetching tasks
+      </p>
+    );
+  }
+
   return (
     <div className="max-w-3xl mx-auto space-y-5">
       <div className="flex items-center justify-between pb-1 border-b border-slate-300">
@@ -18,23 +40,38 @@ function Tasks() {
         </div>
         <TaskForm />
       </div>
-      {isPending ? (
+      {isLoading ? (
         <div className="text-center py-20 flex items-center justify-center">
-          <TbLoader size={30} className="animate-spin" />
+          <TbLoader
+            size={30}
+            color="#64748b"
+            className="animate-spin text-slate-500"
+          />
         </div>
       ) : data && data.pages && data.pages.length > 0 ? (
-        <div ref={parent} className="space-y-3 pb-5">
+        <ul className="space-y-3 pb-5">
           {data.pages.map((page) =>
-            page.map((task: TaskStats, ind: number) => {
-              return <TaskUI key={ind} task={task} />;
+            page.map((task: TaskStats) => {
+              return <TaskUI key={task._id} task={task} />;
             })
           )}
-        </div>
+        </ul>
       ) : (
         <p className="pt-12 text-center font-mono tracking-widest text-slate-400 text-2xl font-semibold">
           No Task yet!
         </p>
       )}
+      <div ref={ref}>
+        {isFetchingNextPage && hasNextPage && (
+          <div className="text-center pb-3 flex items-center justify-center">
+            <TbLoader
+              size={30}
+              color="#64748b"
+              className="animate-spin text-slate-500"
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
